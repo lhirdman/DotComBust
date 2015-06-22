ba8e8c4fcfa75f71624adf6a5c41aea843d0a642
Love Hirdman <love.hirdman@gmail.com>
2015-06-22 09:28:47 +0200

    Added some more

---
+++ src/DotCom.java
@@ -0,0 +1,5 @@
+import java.util.*;
+public class DotCom {
+	private ArrayList<String> locationCells;
+	private String name;
+}
---
+++ src/DotComBust.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class DotComBust {
+	private GameHelper helper = new GameHelper();
+	private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+	private int numOfGuesses = 0;
+	private void setUpGame() {
+		DotCome one = new DotCom();
+		one.setName
+	}
+}
